bd885a723336b7dd638fcac86d59fc3351adaf47
pccjamie <pccjamie@gmail.com>
2013-08-01 18:04:25 -0400

    Troubleshooting db migration issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
   end
 
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
+
+  #relies on protected attributes gem. UntiL I figure out how to customize Devise controller directly which is hidden inside gem apparenty.
+ 
+attr_accessible  :name, :email, :password, :password_confirmation, :remember_me, :provider, :uid, :location, :picture, :first_name, :city, :state
+
  
 before_filter :authenticate_user!
 before_filter :user_signed_in?
@@ -11,9 +16,9 @@ before_filter :user_signed_in?
 
 # FOR WHEN I SWITCH TO USING STORNG PARMETERS
 
-  # private
-  #   def user_params
-  #     params.require(:person).permit(:provider, :uid, :name)
-  #   end
+  private
+    def user_params
+      params.require(:person).permit(:provider, :uid, :name)
+    end
 
 end
