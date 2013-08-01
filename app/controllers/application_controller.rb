2fcc7afa74de49dbf26b310d9a53f08af31f23f6
pccjamie <pccjamie@gmail.com>
2013-08-01 18:27:15 -0400

    Troubleshooting db migration issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
          devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email, :location, :picture) }
          devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :location, :picture) }
-         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :location, :picture }
+         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :location, :picture) }
   end
 
 
