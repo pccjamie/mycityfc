2f8ae1600a75a02efd387279921abb70a571903f
pccjamie <pccjamie@gmail.com>
2013-08-01 19:19:27 -0400

    Pushing up customization of devise parameter sanitization functionality.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :configure_permitted_parameters, if: :devise_controller?
+  before_filter :devise_parameter_sanitizer, if: :devise_controller?
 
+  #relies on lib/user_sanitizer.rb
   protected
-
-
-
-  def configure_permitted_parameters
-         devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email, :location, :picture) }
-         devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation, :location, :picture) }
-         devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password, :location, :picture) }
-  end
-
-
+	
+	def devise_parameter_sanitizer
+	    if resource_class == User
+	      User::ParameterSanitizer.new(User, :user, params)
+	    else
+	      super
+	    end
+	end
 
 end
---
+++ lib/user_sanitizer.rb
@@ -0,0 +1,20 @@
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+
+	#these add on to the behavior provided by the Devise parameter sanitizer
+   
+    private
+
+    def sign_up
+    	default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state)
+    end
+
+    def sign_in
+        default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state)
+    end
+
+    def account_update
+        default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state)
+    end
+
+end
+
