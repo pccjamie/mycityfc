fe9ec85b125723fc0e854109e551e9188aa9aa4d
pccjamie <pccjamie@gmail.com>
2013-08-01 17:34:28 -0400

    Troubleshooting db migration issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
  
-# before_filter :authenticate_user!
-# before_filter :user_signed_in?
+before_filter :authenticate_user!
+before_filter :user_signed_in?
 
   def index
   	p'you are on the controller page'
