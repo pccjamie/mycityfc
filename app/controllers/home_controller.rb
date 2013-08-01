8ca2fdc4572f0677784b06e1c3d89bbbcc54fb7e
pccjamie <pccjamie@gmail.com>
2013-08-01 18:10:13 -0400

    Troubleshooting db migration issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
 
-  #relies on protected attributes gem. UntiL I figure out how to customize Devise controller directly which is hidden inside gem apparenty.
- 
-attr_accessible  :name, :email, :password, :password_confirmation, :remember_me, :provider, :uid, :location, :picture, :first_name, :city, :state
-
  
 before_filter :authenticate_user!
 before_filter :user_signed_in?
@@ -16,9 +12,9 @@ before_filter :user_signed_in?
 
 # FOR WHEN I SWITCH TO USING STORNG PARMETERS
 
-  private
-    def user_params
-      params.require(:person).permit(:provider, :uid, :name)
-    end
+  # private
+  #   def user_params
+  #     params.require(:person).permit(:provider, :uid, :name)
+  #   end
 
 end
