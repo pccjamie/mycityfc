ab6a3acde4e488b58874a425ea429e494b1cbb19
pccjamie <pccjamie@gmail.com>
2013-08-01 17:04:50 -0400

    DB migration to correct auth isues.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130801162831) do
+ActiveRecord::Schema.define(version: 20130801210131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,9 @@ ActiveRecord::Schema.define(version: 20130801162831) do
     t.string   "name"
     t.string   "location"
     t.string   "picture"
+    t.string   "city"
+    t.string   "state"
+    t.string   "first_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
