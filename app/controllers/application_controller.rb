6aa1bdb5962d8934fef4cad42b393cc97555e4af
pccjamie <pccjamie@gmail.com>
2013-08-02 00:49:21 -0400

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :devise_parameter_sanitizer, if: :devise_controller?
-
-  #relies on lib/user_sanitizer.rb
-  protected
-	
-	def devise_parameter_sanitizer
-	    if resource_class == User
-	      User::ParameterSanitizer.new(User, :user, params)
-	    else
-	      super
-	    end
-	end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,29 @@ class HomeController < ApplicationController
 before_filter :authenticate_user!
 before_filter :current_user
 
+
+  before_filter :devise_parameter_sanitizer, if: :devise_controller?
+
+	
+
   def index
   	p'you are on the controller page'
   end
+
+
+
+
+  #relies on lib/user_sanitizer.rb
+  protected
+	
+	def devise_parameter_sanitizer
+	    if resource_class == User
+	      User::ParameterSanitizer.new(User, :user, params)
+	    else
+	      super
+	    end
+	end
+
 
 
 
