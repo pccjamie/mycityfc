dab4aa44821d670a75e4dc9ee2c3be363dec192e
pccjamie <pccjamie@gmail.com>
2013-08-01 20:33:11 -0400

    Testing fix. Duplicate omniauth callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,16 @@ devise_for :users,:controllers => { :omniauth_callbacks => "users/omniauth_callb
 # # only using omniauth for authentication, so added these session routes within user scope. 
 # note that sessions was ALSO skipped in the normal devise resources.
 
-# devise_scope :user do
-#   get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
-#   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-# end
+devise_scope :user do
+  get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+end
 
 root :to => "home#index"
 
 #rest  
 resources :home, :only => [:index]
 
-resources :users
+
 
 end
