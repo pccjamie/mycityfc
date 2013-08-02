38823c268295a4b56d6d6cb611467a4e0f54f638
pccjamie <pccjamie@gmail.com>
2013-08-01 21:15:25 -0400

    Testing fix. Duplicate omniauth callbacks.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Soccer1::Application.routes.draw do
   
 devise_for :users, :skip => [:sessions], :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-
 # only using omniauth for authentication, so added these session routes within user scope. 
 # note that sessions was ALSO skipped in the normal devise resources.
 
