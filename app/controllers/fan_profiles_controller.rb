cfc19561a1e7730cc11a8ac692516573ea8889f0
pccjamie <pccjamie@gmail.com>
2013-08-06 17:07:05 -0400

    Reworked auth flow. Now requests profile and saves to DB prior to displaying home page for current user.

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -3,15 +3,22 @@ class FanProfilesController < ApplicationController
 
 
 
- #  def index
- #  end
+  def index
+  end
 
- #  def show
- #  end
+  def show
 
- #  def get_location
-	# @city = current_user.city
- #  end
+
+
+  end
+
+  def get_location
+	# city = current_user.city
+
+ #  	if city == 
+
+
+  end
 
 
 
