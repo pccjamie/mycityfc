b4df9bc5a3f856b54097d4d50ddd894312a200d7
pccjamie <pccjamie@gmail.com>
2013-08-06 15:05:05 -0400

    testing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,12 +7,13 @@ end
 
 def update
 
+  @user = User.find(params[:id])
+
 	if params[:user][:password].blank?
       params[:user].delete("password")
       params[:user].delete("password_confirmation")
     end
   
-  @user = User.find(params[:id])
   @user.update_attributes!(profile_params)
   super
 end
