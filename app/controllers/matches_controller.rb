37638c7eaeb977d95803cbee1b7cc1d6693c6238
pccjamie <pccjamie@gmail.com>
2013-08-06 12:12:21 -0400

    testing

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,49 +2,54 @@ class MatchesController < ApplicationController
 
 
 
-#require 'httparty'
+require 'httparty'
 
-#before_filter :authenticate_user!
-
-
-# 	def index
-# 		@matches = Match.all
-# 	end
-
-# 	def new
-# 		@match = Match.new
-# 	end
-
-# 	# def create
-# 	# 	@match = Match.create(match_params)
-# 	# 	if @match.save
-# 	# 	redirect_to matches_path
-# 	# 	else
-# 	# 	redirect_to new_match_path
-# 	# 	end
-# 	# end
-
-# 	def show
-#   		@match = Match.find(match_params)  		
-# 	end
-
-# 	def edit
-# 	end
-
-# 	def update
-# 	end
-
-# 	def destroy
-# 		#needs logic for removing match info after a certain amount of time
-# 	end
+before_filter :authenticate_user!
 
 
-# 	#if i use strong params now and remove attr_accessible from model, will that 
+	def index
+		@matches = Match.all
+	end
 
-# 	private
-#       def match_params
-#         params.require(:match).permit!
-#       end
+	def new
+		@match = Match.new
+	end
+
+	def create
+		@match = Match.create(match_params)
+		if @match.save
+		redirect_to matches_path
+		else
+		redirect_to new_match_path
+		end
+	end
+
+	def show
+  		@match = Match.find(match_params)  		
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+
+
+
+
+
+	def destroy
+		#needs logic for removing match info after a certain amount of time
+	end
+
+
+	#if i use strong params now and remove attr_accessible from model, will that 
+
+	private
+      def match_params
+        params.require(:match).permit!
+      end
 
 
 
