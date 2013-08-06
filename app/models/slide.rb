8269ab1806a5c365e3160708939aa8fc85a970fe
pccjamie <pccjamie@gmail.com>
2013-08-05 21:12:52 -0400

    testing rake task for scraping

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,5 +1,13 @@
+require 'rake'
+
 class Slide < ActiveRecord::Base
 
-# attr_accessible :name, :desc, :image_url, :cite
+	def self.run_rake(scrape)
+    	load File.join(RAILS_ROOT, 'lib', 'tasks', 'scrape.rake')
+    	Rake::Task[scrape].invoke
+    	Rake::Task[scrape].reenable
+
+	end
+
 
 end
