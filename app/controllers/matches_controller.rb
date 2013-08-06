d3fb7ce4e519e7968ec2980bcf12cf270951baef
pccjamie <pccjamie@gmail.com>
2013-08-06 12:21:19 -0400

    testing

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,54 +2,54 @@ class MatchesController < ApplicationController
 
 
 
-require 'httparty'
+# require 'httparty'
 
-before_filter :authenticate_user!
-
-
-	def index
-		@matches = Match.all
-	end
-
-	def new
-		@match = Match.new
-	end
-
-	def create
-		@match = Match.create(match_params)
-		if @match.save
-		redirect_to matches_path
-		else
-		redirect_to new_match_path
-		end
-	end
-
-	def show
-  		@match = Match.find(match_params)  		
-	end
-
-	def edit
-	end
-
-	def update
-	end
+# before_filter :authenticate_user!
 
 
+# 	def index
+# 		@matches = Match.all
+# 	end
+
+# 	def new
+# 		@match = Match.new
+# 	end
+
+# 	def create
+# 		@match = Match.create(match_params)
+# 		if @match.save
+# 		redirect_to matches_path
+# 		else
+# 		redirect_to new_match_path
+# 		end
+# 	end
+
+# 	def show
+#   		@match = Match.find(match_params)  		
+# 	end
+
+# 	def edit
+# 	end
+
+# 	def update
+# 	end
 
 
 
 
-	def destroy
-		#needs logic for removing match info after a certain amount of time
-	end
 
 
-	#if i use strong params now and remove attr_accessible from model, will that 
+# 	def destroy
+# 		#needs logic for removing match info after a certain amount of time
+# 	end
 
-	private
-      def match_params
-        params.require(:match).permit!
-      end
+
+# 	#if i use strong params now and remove attr_accessible from model, will that 
+
+# 	private
+#       def match_params
+#         params.require(:match).permit!
+#       end
 
 
 
--- lib/user_sanitizer.rb
+++ lib/user_sanitizer.rb
@@ -5,16 +5,16 @@ class User::ParameterSanitizer < Devise::ParameterSanitizer
     private
 
     def sign_up
-    	default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state)
+    	default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state, :profile)
     end
 
     # def sign_in
     #     default_params.permit(:first_name, :name, :picture, :provider, :uid, :remember_me, :email, :password, :password_confirmation, :current_password, :city, :state)
     # end
 
-    def account_update
-        default_params.permit(:city, :state)
-    end
+    # def account_update
+    #     default_params.permit(:city, :state)
+    # end
 
 end
 
