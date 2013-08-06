938796692633ef204d66d2368494424d341c23d0
pccjamie <pccjamie@gmail.com>
2013-08-06 13:48:04 -0400

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_filter :devise_parameter_sanitizer, if: :devise_controller?
 
 	def after_sign_in_path_for(user)
-    	edit_user_registration_path
+    	users_edit_path
 	end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Soccer1::Application.routes.draw do
   
-devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "users/registrations" }
+devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+get "users/edit", :to => "users/registrations#edit"
 
 #only using omniauth for authentication, so added these session routes within user scope. 
 #note that sessions was ALSO skipped in the normal devise resources.
@@ -12,12 +14,10 @@ devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_call
 # end
 
 resources :home, :only => [:index]
-
 root :to => "home#index"
 
 resources :slides
 
-# get "users/edit", :to => "users/registrations#edit"
 
 # resources :matches
 # resources :profiles
