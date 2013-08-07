a29d3b890baca1553f3b5e47147717b41af1aba5
pccjamie <pccjamie@gmail.com>
2013-08-07 02:55:35 -0400

    testing

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -1,7 +1,7 @@
 class FanProfilesController < ApplicationController
   require 'nokogiri'
   require 'open-uri'
-
+  require 'httparty'
 
 
   def get_weather
@@ -12,9 +12,8 @@ class FanProfilesController < ApplicationController
       formatted_state = user_state.gsub!(/ /,"")
       raw_city = current_user.city
       user_city = raw_city.gsub!(/ /, "+")
-      @url = "www.wunderground.com/weather-forecast/US/#{formatted_state}/#{user_city}.html"
-      @temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
-    	@temp
+      url = "www.wunderground.com/weather-forecast/US/#{formatted_state}/#{user_city}.html"
+      temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
     # # rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
   end
 
@@ -23,6 +22,7 @@ class FanProfilesController < ApplicationController
   def index
     # @user = User.find(current_user.id)
     get_weather
+    puts temp
     return
   end
 
@@ -30,6 +30,8 @@ class FanProfilesController < ApplicationController
   end
 
   def get_league
+
+  	url = HTTP GET http://api.espn.com/:version/:resource/:method?apikey=:yourkey
   	#either get json or nokogiri scrape of MLS. Find retrieved tteams locations. USe geocoder to compare to user location
   end
 
