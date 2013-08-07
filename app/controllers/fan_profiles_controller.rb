498a15ad45ef48110ceb23aaebb61f5c63d5391f
pccjamie <pccjamie@gmail.com>
2013-08-07 03:19:53 -0400

    testing

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -5,6 +5,7 @@ class FanProfilesController < ApplicationController
 
 
   def get_weather
+  	  @user = User.find(current_user.id)
       raw_state = current_user.state
       user_state = raw_state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
       formatted_state = user_state.gsub!(/ /,"")
@@ -19,14 +20,15 @@ class FanProfilesController < ApplicationController
 
 
   def index
-   	@user = User.find(current_user.id)
-   	get_weather
+   	puts @temp
   end
 
   def show
   end
 
   def get_league
+
+
 
   	# url = JSONHTTP GET "http://api.espn.com/:version/:resource/:method?apikey=:yourkey"
   	#either get json or nokogiri scrape of MLS. Find retrieved tteams locations. USe geocoder to compare to user location
