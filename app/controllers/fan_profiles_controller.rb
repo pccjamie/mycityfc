4d178bae9c8c2341280e8a787ed7eac9a309191c
pccjamie <pccjamie@gmail.com>
2013-08-07 02:24:32 -0400

    testing

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -1,13 +1,13 @@
 class FanProfilesController < ApplicationController
-require 'nokogiri'
-require 'open-uri'
+  require 'nokogiri'
+  require 'open-uri'
 
   def index
-  	@user = User.find(current_user.id)
-  	puts current_user
-  	get_weather
-  	return @temp
-end
+    @user = User.find(current_user.id)
+    puts current_user
+    get_weather
+    return @temp
+  end
 
   def show
   end
@@ -20,26 +20,23 @@ end
   end
 
 
-	def get_weather
-		if current_user.city
-		# @user = User.find(current_user.id)
-		raw_state = current_user.state
-		user_state = raw_state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
-		puts user_state
-		raw_city = current_user.city
-		user_city = raw_city.gsub!(/ /, "+")
-	 	url = "www.wunderground.com/weather-forecast/US/#{user_state}/#{user_city}.html"
-		@temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
-		else
-			render :nothing
-		end
-		# return
-		# puts temp
-		# # rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
-	end
+  def get_weather
+      # @user = User.find(current_user.id)
+      raw_state = current_user.state
+      user_state = raw_state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
+      formatted_state = user_state.gsub!(/ /,"")
+      raw_city = current_user.city
+      user_city = raw_city.gsub!(/ /, "+")
+      url = "www.wunderground.com/weather-forecast/US/#{formatted_state}/#{user_city}.html"
+      @temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
+  
+    # return
+    # puts temp
+    # # rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
+  end
 
-private
-      def user_params
-        params.require(:user).permit!
-      end     
+  private
+  def user_params
+    params.require(:user).permit!
+  end
 end
