b08f7079d4c8c990d0a93396f3627e6859047367
pccjamie <pccjamie@gmail.com>
2013-08-07 01:11:28 -0400

    testing

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -7,20 +7,17 @@ require 'open-uri'
   	puts current_user
   	get_weather
   	puts @temp 
-
   end
 
   def show
   end
-
 
 	def get_weather
 		# @user = User.find(current_user.id)
 		raw_city = current_user.city
 		raw_state = current_user.state
 		user_city = raw_city.gsub!(/ /, "+")
-		user_state = raw_state.gsub!(/\s+/, "")
-		user_state = user_state.gsub!(/[A-Z]{2}/, "+")
+		user_state = raw_state.gsub!(/\s+{2}/, "+")
 
 	 	url = "www.wunderground.com/weather-forecast/US/#{user_state}/#{user_city}.html"
 		@temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
