b65defa147b0e8e9a0b6a37a5dc76ca91de313bd
pccjamie <pccjamie@gmail.com>
2013-08-07 11:48:41 -0400

    testing

--- app/assets/javascripts/soccer.js
+++ app/assets/javascripts/soccer.js
@@ -1,17 +1,19 @@
 // var city = $('.current-user-city').text();
-var data;
+var url = 'http://api.espn.com/v1/sports/soccer/usa.1/?apikey=4u3e6enmscdszh8qcy9dh7my';
 function espn_find_leagues(){
-	
-	console.log('here i am')
-	// $.ajax({
-	// 	url: 'http://api.espn.com/v1/sports/soccer/usa.1/?apikey=4u3e6enmscdszh8qcy9dh7my',
-	// 	dataType: json,
-	// 	success: function(data) {
-	// 		my_data = data;
-	// 		$('#my-leagues').append(+my_data+)
-	// 	}
-	// })
+	console.log('here i am');
+	$.ajax({
+		dataType: "json",
+		url: url,
+		data: data,
+		success: function(data) {
+			console.log(data);
+			var my_data = data;
+			$('#my-leagues').append(my_data);
+		}
+	});
 }
+
 //DOM events
 $(function(){
 	espn_find_leagues();
