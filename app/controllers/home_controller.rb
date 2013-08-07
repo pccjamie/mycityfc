2dd742119a334462ac140ebfc2032b7605024452
pccjamie <pccjamie@gmail.com>
2013-08-06 20:05:40 -0400

    Route updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 	
 # skip_before_filter :authenticate_user! , :only => [:index]
 
-before_filter :authenticate_user!, :only => [:index]
+before_filter :authenticate_user!
 
 before_filter :current_user
 
