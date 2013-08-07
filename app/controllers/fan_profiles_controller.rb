34f5ebd734c10a143f79e4abe82fab0ccf483369
pccjamie <pccjamie@gmail.com>
2013-08-07 09:51:29 -0400

    testing

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -17,13 +17,13 @@ class FanProfilesController < ApplicationController
   end
 
   def get_weather
+  	@user = User.find(user_params)
     @weather = "showing weather"
-
     state = current_user.state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
     state = state.gsub!(/ /,"")
-    city = current_user.city.gsub!(/ /, "+")
+    # city = current_user.city.gsub!(/ /, "+")
     @state = state 
-    @city = city
+    # @city = city
     # url = "www.wunderground.com/weather-forecast/US/#{state}/#{user_city}.html"
     # puts @url
     #  # @temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
