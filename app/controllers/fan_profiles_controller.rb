56fcd479fc2357480dffc288d08a954de18acb8f
pccjamie <pccjamie@gmail.com>
2013-08-06 21:20:24 -0400

    Weather scrape

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -1,15 +1,13 @@
 class FanProfilesController < ApplicationController
-  
-
-
+require 'nokogiri'
+require 'open-uri'
 
   def index
+  	get_weather
+  	return
   end
 
   def show
-
-
-
   end
 
   def get_location
@@ -19,6 +17,17 @@ class FanProfilesController < ApplicationController
 
 
   end
+
+	def get_weather
+	
+	    url = "http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=#{current_user.city}"
+
+		temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
+		rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
+
+		return
+
+	end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
-class UsersController < Devise::RegistrationsController
+# class UsersController < Devise::RegistrationsController
 
-def update
-  @user = User.find(params[:id])
-  @user.update_attributes!(profile_params)
-  super
-end
+# def update
+#   @user = User.find(params[:id])
+#   @user.update_attributes!(profile_params)
+#   super
+# end
 
-private
-      def profile_params
-        params.require(:user).permit(:profile)
-      end
+# private
+#       def profile_params
+#         params.require(:user).permit(:profile)
+#       end
       
-end
+# end
