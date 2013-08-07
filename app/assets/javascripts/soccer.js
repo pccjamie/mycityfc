3655ba9bedd57238a6a5dc8921372f404d0a8466
pccjamie <pccjamie@gmail.com>
2013-08-07 13:58:13 -0400

    testing

--- app/assets/javascripts/soccer.js
+++ app/assets/javascripts/soccer.js
@@ -1,24 +1,25 @@
-var url = 'http://api.espn.com/v1/sports/soccer/usa.1/teams/'+city+'/?apikey=4u3e6enmscdszh8qcy9dh7my';
+var url = 'http://api.espn.com/v1/sports/soccer/usa.1/teams/' + city + '/?apikey=4u3e6enmscdszh8qcy9dh7my';
 var city = $('.current-user-city').text();
-function espn_find_leagues(){
+
+function espn_find_leagues() {
 	$.ajax({
 		dataType: "JSON",
 		cache: false,
 		type: "GET",
 		url: url,
-		success: function(data){
+		success: function(data) {
 			var results = data;
 			console.log(results);
 			results = JSON.stringify(results);
-			$("<div></div>").append(results);
-			// $("#my-leagues").append(results);
-			$("<div>"+ results + "</div>").appendTo("#my-leagues").html();
 			console.log(city);
+			local_results = $(results).find(city);
+			$("<div></div>").append(local_results);
+			// $("#my-leagues").append(results);
+			$("<div>" + local_results + "</div>").appendTo("#my-leagues").html();
 		}
 	});
 }
-
 //DOM events
-$(function(){
+$(function() {
 	espn_find_leagues();
 });
