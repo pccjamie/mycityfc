7cfb3220231652fef72983ed57777327afe73432
pccjamie <pccjamie@gmail.com>
2013-08-07 09:35:18 -0400

    testing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,20 +7,20 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
  def update
     # required for settings form to submit when password is left blank
-
+    current_user
     if params[:user][:password].blank?
       params[:user].delete("password")
       params[:user].delete("password_confirmation")
     end
 
-    @user = User.find(profile_params)
+    @user = User.find(current_user.id)
     if @user.update_attributes(profile_params)
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case his password changed
       sign_in @user, :bypass => true
       redirect_to after_update_path_for(@user)
     else
-      render "users#edit"
+      render "edit"
     end
   end
 
