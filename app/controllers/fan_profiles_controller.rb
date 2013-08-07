ae59517e488a3351cce5a297b08165ddc818bc57
pccjamie <pccjamie@gmail.com>
2013-08-06 23:01:59 -0400

    Fixes

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -4,32 +4,25 @@ require 'open-uri'
 
   def index
   	get_weather
-  	return
   end
 
   def show
   end
 
-  def get_location
-	# city = current_user.city
-  end
 
 	def get_weather
-		@user = User.find(current_user.id)
+		@user = User.find(current_user.user_params)
+		# @user = User.find(current_user.id)
 		raw = current_user.city
 		user_city = raw.gsub!(/ /, '+')
 		puts user_city
 	 #    url = "http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=#{city}"
 		# temp = Nokogiri.HTML(open(url).css('#nowTemp, b')).to_html
 		# # rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
-		return
 	end
 
-
-
 private
-      def profile_params
-        params.require(:user).permit(:city)
+      def user_params
+        params.require(:user).permit!
       end     
-
 end
