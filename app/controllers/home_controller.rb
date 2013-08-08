75179dc566029316d33e9f4e93afa7d68726a74e
pccjamie <pccjamie@gmail.com>
2013-08-08 14:23:45 -0400

    Adding images. DRY code, moved conditionals into helper. New parents profile page layout. rewrote bg image load script.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 	
 # skip_before_filter :authenticate_user!, :only => [:index]
 before_filter :authenticate_user!
+before_filter :user_signed_in?
 
 	def index
    #  current_user
