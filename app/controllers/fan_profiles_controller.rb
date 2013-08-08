f7a1818fa975231f060dc499e2e22499fa9dda50
pccjamie <pccjamie@gmail.com>
2013-08-07 22:23:40 -0400

    Weather scrape. Added news feed. CSS updates. Starting match day panel set up

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -27,8 +27,9 @@ class FanProfilesController < ApplicationController
   end
 
   def get_weather
-    # state = current_user.state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
-    # state = state.gsub!(/ /,"")
+    state = current_user.state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
+    state = state.gsub!(/ /,"")
+    @state = state
     # # city = current_user.city.gsub!(/ /, "%20")
     # # url = "http://wunderground.com/weather-forecast/US/#{state}/#{city}.html"
     # weather = Nokogiri::HTML(open("http://weather.weatherbug.com/#{state}/#{city}-weather.html")).css("#divTemp").text
