845e09d1a0fe437416b040ef8aaf36312250ecff
pccjamie <pccjamie@gmail.com>
2013-08-08 19:50:22 -0400

    Working on app helper for conditional CSS on body tag

---
+++ app/assets/javascripts/swap_bg.js
@@ -0,0 +1,13 @@
+function swap_bg() {
+
+	if $('body').hasClass('bg-fan-profiles'); {
+		$(this).removeClass('bg-fan-profiles').addClass('bg-parent-profiles');
+	} else {
+		$(this).removeClass('bg-fan-parents').addClass('bg-fan-profiles');
+	}
+	return;
+}
+
+$(function() {
+	$('#utility #lower').on('click', "#lower ul li a", swap_bg);
+});
