5cbd5581234ee9e0347b23d9d94a0a40060315a2
pccjamie <pccjamie@gmail.com>
2013-08-08 00:17:11 -0400

    Weather scrape. Added news feed. CSS updates. Starting match day panel set up

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -1,13 +1,11 @@
 class FanProfilesController < ApplicationController
 
+  def set_access_control_headers
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Request-Method"] = "*"
+  end
 
-  # def set_access_control_headers
-  #   headers["Access-Control-Allow-Origin"] = "*"
-  #   headers["Access-Control-Request-Method"] = "*"
-  # end
-
-  # after_filter :set_access_control_headers
-
+  after_filter :set_access_control_headers
 
 # uncomment for  localhost testing
 # skip_before_filter :authenticate_user!
@@ -17,7 +15,6 @@ class FanProfilesController < ApplicationController
   require 'httparty'
 
   def index
-  	@user = User.find(current_user.id)
     get_weather
     get_leagues
     get_news
@@ -27,13 +24,15 @@ class FanProfilesController < ApplicationController
   end
 
   def get_weather
-    state = current_user.state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
-    state = state.gsub!(/ /,"")
-    city = current_user.city.gsub!(/ /, "%20")
-   	# url = "http://wunderground.com/weather-forecast/US/#{state}/#{city}.html"
-    weather = Nokogiri::HTML(open("http://weather.weatherbug.com/#{state}/#{city}-weather.html")).css("#divTemp").text
+  	current_user
+  	state = current_user.state
+  	city = current_user.city
+    # state = current_user.state.gsub!(/\b\s\b/, "+").gsub!(/\b/, "")
+    # state = state.gsub!(/ /,"")
+    # city = current_user.city.gsub!(/ /, "%20")
+    # weather = Nokogiri::HTML(open("http://weather.weatherbug.com/#{state}/#{city}-weather.html")).css("#divTemp").text
+    weather = Nokogiri::HTML(open("http://weather.weatherbug.com/NY/New%20York-weather.html")).css("#divTemp").text
     @weather = weather
-    # # rain = Nokogiri.HTML(open(url).css('#curCond , #conds_details_cur, b')).to_html
   	return
   end
 
