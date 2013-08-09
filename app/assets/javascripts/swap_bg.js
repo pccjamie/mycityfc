d4000a89988719da421ee78b5eeb878709aa6c3c
pccjamie <pccjamie@gmail.com>
2013-08-08 20:08:42 -0400

    Adding JS to manage bg image changes.

--- app/assets/javascripts/swap_bg.js
+++ app/assets/javascripts/swap_bg.js
@@ -1,15 +1,14 @@
 function swap_bg() {
 
-	if ($('body').hasClass('bg-fan-profiles'))
-	{
+	if ($('body').hasClass('bg-fan-profiles')){
 		$(this).removeClass('bg-fan-profiles').addClass('bg-parent-profiles');
 	}
-	else {
+	if ($('body').hasClass('bg-parent_profiles')) {
 		$(this).removeClass('bg-fan-parents').addClass('bg-fan-profiles');
 	}
 	return;
 }
 
 $(function() {
-	$('#utility #lower').on('click', "#lower ul li a", swap_bg);
+	$('#utility-bar #lower').on('click', "ul li a", swap_bg);
 });
