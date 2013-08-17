ff64e96f36b7a7f6bcbf1dc47d95c49188123394
pccjamie <pccjamie@gmail.com>
2013-08-17 17:00:19 -0400

    First batch of Content filtering based on loc working now.

--- app/assets/javascripts/soccer.js
+++ app/assets/javascripts/soccer.js
@@ -1,43 +1,43 @@
-// var city = $('.current-user-city').text();
-var my_location = "Seattle";
+var city = $('li.current-user-city').html();
+var city = city.toLowerCase();
 var i = 0;
-// var results;
-// var match;
-// var record;
 
-// FIND YOUR TEAM
+// GET FIND YOUR TEAM
+function espn_find_team() {
+	var source_teams = 'http://api.espn.com/v1/sports/soccer/usa.1/teams/';
+	// console.log('entered function - espn find team');	
+	$.ajax({
+		url: source_teams,
+		data: {
+			apikey: '4u3e6enmscdszh8qcy9dh7my',
+			_accept: "application/json"
+		},
+		dataType: "jsonp",
+		beforeSend: function(xhr) {
+			xhr.setRequestHeader("Accept", "application/json");
+		},
+		cache: false,
+		type: "get"
+	}).done(function(data) {
+		$.each(data.sports[0].leagues[0].teams, function(index, team) {
+			// console.log(index + ': ' + team.location + '-' + team.name);
+			var click;
+			
+		
+		$('#my-teams * header').append("<div class=team>" + team.location + ' ' + team.name + "</div>");
 
-// function espn_find_team() {
-// 	var source_teams = 'http://api.espn.com/v1/sports/soccer/usa.1/teams/';
-// 	// console.log('entered function - espn find team');
-	
-// 	$.ajax({
-// 		url: source_teams,
-// 		data: {
-// 			apikey: '4u3e6enmscdszh8qcy9dh7my',
-// 			_accept: "application/json"
-// 		},
-// 		dataType: "jsonp",
-// 		beforeSend: function(xhr) {
-// 			xhr.setRequestHeader("Accept", "application/json");
-// 		},
-// 		cache: false,
-// 		type: "get"
-// 	}).done(function(data) {
-// 		$.each(data.sports[0].leagues[0].teams, function(index, team) {
-// 			console.log(index + ': ' + team.location + '-' + team.name);
-// 			var click;
-// 			$('#my-teams .sleeve').append("<div class=team>" + this.location + ' ' + this.name + "</div>");
-// 		});
-
-// 	});
-// }
+		$("#my-teams * .team:contains('"+city+"')").css("display","block" );
 
 
+		});
+	});
+}
+
+// GET INFO
 function find_team_info() {
 	var espn = 'http://api.espn.com/v1/sports/soccer/usa.1/teams/links/web/';
 	// console.log('entered function - espn find team');
-	
+	var espn_links;
 	$.ajax({
 		url: espn,
 		data: {
@@ -49,72 +49,77 @@ function find_team_info() {
 			xhr.setRequestHeader("Accept", "application/json");
 		},
 		cache: false,
-	type: "get"
-}).done(function(data) {
+		type: "get"
+	}).done(function(data) {
 
-	$.each(data.sports[0].leagues[0].teams, function(index,team){
-				$.each(team, function(index, info){
-						// console.log('Raw: ' + info);
-						info = $(info);
-						info.slice(5,7);
-						// console.log('Result after splice: ' + info);
-						$.each(info, function(index, linkset){
-								// console.log(urls);
-									$.each(linkset, function(index, set){
-										// console.log(set);
-										$.each(set, function(index, contents){
-											// console.log(singleurl);
-											$.each(contents, function(index, cheese){
-												// console.log(onelink);
-												console.log(cheese);
+		$.each(data.sports[0].leagues[0].teams, function(index, team) {
 
-													$('#my-teams * nav').append("<div>" + cheese + "</div>");
+			$.each(team, function(index, info) {
+				
+				info2 = $(info);
+				info2.slice(5, 7);
+				$.each(info2, function(index, linkset) {
+					$.each(linkset, function(index, set) {
+						// console.log(set);
+						$.each(set, function(index, contents) {
+							$.each(contents, function(index, espn_links) {
 
-											});
-										});
-									});
-								
-								
+
+							$('<a class="team-link" href='+espn_links+'>'+ espn_links +'</a>').appendTo('#my-teams * nav');
+		
+							$("#my-teams * a:contains('usa.1')").remove();
+							$("#my-teams * a:contains('"+city+"')").css("display","block" );
 					
+						
+					
+							$("#my-teams * br").remove();
+						
+							});
 						});
+					});
 				});
 			});
 		});
-	}
+	});
+	return;
+}
 
 
-		// // GET NEWS
 
-		// function espn_find_news() {
-		// 	$.ajax({
-		// 		url: 'http://api.espn.com/v1/sports/soccer/usa.1/news/',
-		// 		data: {
-		// 			apikey: "4u3e6enmscdszh8qcy9dh7my",
-		// 			_accept: "application/json"
-		// 		},
-		// 		dataType: "jsonp",
-		// 		beforeSend: function(xhr) {
-		// 			xhr.setRequestHeader("Accept", "application/json");
-		// 		},
-		// 		cache: false,
-		// 		type: "get"
-		// 	}).done(function(data) {
-		// 		raw = data;
-		// 		// $.each(raw.headlines[0].categories[2], function(index, team) {
-		// 		// console.log('This team has a headline: ' + team.headline);
+		// console.log(city);
 
-		// 		$.each(raw.headlines, function(index, team) {
-		// 			var info = [raw.headlines];
-		// 			// var info = [raw.headlines[0].categories[2].teams];
-		// 			var test = $(this).attr("class", "team-headline");
-		// 			$('#my-news .sleeve').append("<div class=team-headline>" + this.headline + "</div>");
-		// 		});
-		// 	});
-		// }
 
-		// on load events
+		//GET NEWS
+		function espn_find_news() {
+			$.ajax({
+				url: 'http://api.espn.com/v1/sports/soccer/usa.1/news/',
+				data: {
+					apikey: "4u3e6enmscdszh8qcy9dh7my",
+					_accept: "application/json"
+				},
+				dataType: "jsonp",
+				beforeSend: function(xhr) {
+					xhr.setRequestHeader("Accept", "application/json");
+				},
+				cache: false,
+				type: "get"
+			}).done(function(data) {
+				raw = data;
+				// $.each(raw.headlines[0].categories[2], function(index, team) {
+				// console.log('This team has a headline: ' + team.headline);
+
+				$.each(raw.headlines, function(index, team) {
+					var info = [raw.headlines];
+					// var info = [raw.headlines[0].categories[2].teams];
+					var test = $(this).attr("class", "team-headline");
+					$('#my-news .sleeve').append("<div class=team-headline>" + this.headline + "</div>");
+				});
+			});
+		}
+
+		// ON LOAD
 		$(function() {
 			//espn_find_team();
 			find_team_info();
-			// espn_find_news();
+			espn_find_news();
 		});
