71db82c157043c0d62f72c8f1e1ddd353dbaaa90
pccjamie <pccjamie@gmail.com>
2013-08-22 17:26:19 -0400

    Slide controller and CSS updates

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,7 +1,17 @@
 class SlidesController < ApplicationController
 
+#before_filter :authenticate_user!
+
+	require 'rubygems'
+	require 'nokogiri'
+	require 'open-uri'
+
+
 	def index
 		@slides = Slide.all
+
+
+
 	end
 
 	def new
@@ -18,7 +28,16 @@ class SlidesController < ApplicationController
 	end
 
 	def show
-  		@slide = Slide.find(slide_params)  		
+  		@slide = Slide.find(slide_params) 
+  		
+		 @player_youth_reg = Nokogiri::HTML(open("http://www.usyouthsoccer.org/media_kit/keystatistics/")).css('.block p')[0].to_html
+
+		#mls vs us pro
+    @league_natl_rank = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/Major_League_Soccer_attendance#MLS_average_attendance_vs._other_US_major_sports/")).css('table.wikitable').to_html
+
+   #mls vs intl
+    @league_global_rank = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/Major_League_Soccer_attendance")).css('table.wikitable')[3].to_html
+ 		
 	end
 
 	def edit
