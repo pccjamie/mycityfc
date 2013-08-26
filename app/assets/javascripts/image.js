a301521b500a0eb44e9c957c884c302622133c85
pccjamie <pccjamie@gmail.com>
2013-08-26 16:36:25 -0400

    Updates to seed file

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,9 +1,9 @@
-// function image(){
-	
-// }
-
-// // DOM events
-// 	$(function(){
-// 		$('body').css('background-image','none');
-// 		image();
-// 	});
+function image(){
+	var bg = $('a.team-info img').attr('href');
+	$('body').css('background-image','url('+bg+')');
+}
+// DOM events
+	$(function(){
+		$('body').css('background-image','none');
+		image();
+	});
