90e0271e578f669f0889204e4e43ae0ab78362e4
pccjamie <pccjamie@gmail.com>
2013-08-30 13:54:31 -0400

    Changes to JS for ESPN

--- app/assets/javascripts/image.js
+++ app/assets/javascripts/image.js
@@ -1,7 +1,7 @@
 function location_changes(){
 	var bg = $('.team-venue-image:first').text();
 	$('body').css('background-image','url('+bg+')');
-	$('.team:first').after('<h5>Other teams near you</h5>').nextAll().addClass('secondary');
+	$('.team:first').after('<h5>Other teams near you</h5>').nextAll().addClass('secondary').remove('.team-feed,.team-links');
 }
 // DOM events
 	$(function(){
