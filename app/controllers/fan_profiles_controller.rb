03d07783c7af16176dae6704a8f95cdb90cedbea
pccjamie <pccjamie@gmail.com>
2013-09-03 17:43:05 -0400

    Adding search form for team location

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -30,8 +30,7 @@
     # show_leagues
     # show_news
 
-    @myteams = Team.near([current_user.latitude,current_user.longitude], 200)
-   # @myteams = ['Seattle Sounders']
+    @myteams = Team.near([current_user.latitude,current_user.longitude], 300)
     #match day
     show_match_info
 
