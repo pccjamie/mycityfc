a80c1a37cab77d14ecbec678fd527dc333075b23
pccjamie <pccjamie@gmail.com>
2013-09-04 22:11:01 -0400

    CSS updates

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -24,7 +24,7 @@
     # show_leagues
     # show_news
 
-    @teams = Team.near([current_user.latitude,current_user.longitude], 100) 
+    @teams = Team.near([current_user.latitude,current_user.longitude], 300) 
 
     #match day
     show_match_info
