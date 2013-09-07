371851b0d20b4a4a59592309f567334093c5205d
pccjamie <pccjamie@gmail.com>
2013-09-07 18:20:42 -0400

    Changing Devise edit registration view to add profile updating fields

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -14,6 +14,7 @@ before_filter :authenticate_user!
     end
 
     @user = User.find(current_user.id)
+    
     if @user.update_attributes(profile_params)
       set_flash_message :notice, :updated
       # Sign in the user bypassing validation in case his password changed
@@ -25,6 +26,9 @@ before_filter :authenticate_user!
       render "edit"
     end
   end
+
+
+
 
 private
       def profile_params
