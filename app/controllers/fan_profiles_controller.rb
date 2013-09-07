e122faa0216f4f0e610304652f69b98b0b09e648
pccjamie <pccjamie@gmail.com>
2013-09-07 15:49:59 -0400

    Team directory

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -18,7 +18,7 @@
   def index
     current_user
     @users = User.all
-    @teams = Team.geocoded(:order => :distance)
+    @teams = Team.all
     @nearby_teams = Team.near([current_user.latitude,current_user.longitude], 250) 
     
     get_teams
