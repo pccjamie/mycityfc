e8a277b0757d566b425ec81a4f0d8a008e2e46d9
pccjamie <pccjamie@gmail.com>
2013-09-09 15:00:31 -0400

    Working on logic for match display...again

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -115,10 +115,9 @@
     
     @schedule_mls = Nokogiri::HTML(open(url_mls)).css('.schedule-table').to_html
 
-    if @schedule_mls.contents.include?('Seattle')  #or whatever the condition is
+    if @schedule_mls.include?('Seattle')  #or whatever the condition is
       #match_day
       flash[:alert] = "Date and team match. Showing ALL games for this day. Need to filter so it only returns USERS area."
-
     else
       flash[:alert] = "No matches today. Piss off, wanker!"
     end
