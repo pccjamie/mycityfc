7e0398058d28d3e703bf2f810207121e487be2b7
pccjamie <pccjamie@gmail.com>
2013-10-09 17:55:08 -0400

    login fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,7 @@ belongs_to :profilable, :polymorphic => true
       user = User.create(name:auth.extra.raw_info.name,
                          first_name:auth.info[:first_name],
                          provider:auth.provider,
-                         location:auth.info[:location],
-                         #location:auth.extra.raw_info.name,
-                         
+                         location:auth.info[:location]["name"],
                          uid:auth.uid,
                          # city: auth.info[:location].split(',').first,
                          # state: auth.info[:location].split(',').last,
