a79b5c157dbe625cfb7fbfa52f69a43711c27d4d
pccjamie <pccjamie@gmail.com>
2013-10-09 14:02:01 -0400

    nav partials changes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ get "users/edit", :to => "users/registrations#edit"
 
 resources :home, :only => [:index, :splash]
 
-root :to => 'home#index'
+root :to => 'home#splash'
 
 resources :slides, :except => [:destroy]
 resources :fan_profiles, :except => [:destroy]
