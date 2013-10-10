9230a610215976b3e5c46d669cb62097f9b38b24
pccjamie <pccjamie@gmail.com>
2013-10-10 14:04:09 -0400

    login fixes[A

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,7 +7,7 @@ def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      raise request.env["omniauth.auth"].to_yaml
+      #raise request.env["omniauth.auth"].to_yaml
 
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
