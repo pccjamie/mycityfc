ece70fb185b304d0aa056e82009dbcb53372240c
pccjamie <pccjamie@gmail.com>
2013-10-10 13:46:11 -0400

    login fixes[A

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,11 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 def facebook
   
+      # raise request.env["omniauth.auth"].to_yaml
 
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-    raise request.env["omniauth.auth"].to_yaml
-
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
