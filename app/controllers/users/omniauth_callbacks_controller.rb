eec48f516d83bf92dd41f9616034aea0177969a9
pccjamie <pccjamie@gmail.com>
2013-10-10 13:55:43 -0400

    login fixes[A

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,6 +10,8 @@ def facebook
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
+      raise request.env["omniauth.auth"].to_yaml
+
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       #need to change?
       redirect_to root_path
