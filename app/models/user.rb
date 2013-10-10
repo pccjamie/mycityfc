01cefbd2954d58e008faf8197d970daa06746d03
pccjamie <pccjamie@gmail.com>
2013-10-10 10:29:07 -0400

    login fixes[A

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,10 +28,12 @@ belongs_to :profilable, :polymorphic => true
                          # city: auth.info[:location].split(',').first,
                          # state: auth.info[:location].split(',').last,
                          
-                        location:auth.extra.raw_info[:location => [:name]],
+                        #location:auth.extra.raw_info[:location][:name],
+                         location:auth.info.location,
 
 
-                        #LOGIN AND SAVE 
+                        #LOGIN AND SAVE
+                        #location:auth.extra.raw_info[:location => [:name]] 
                         #location:auth.extra.raw_info["gender"],# GOOD SAVE, RIGHT HASH
                         #location:auth.extra.raw_info[:gender]
 
