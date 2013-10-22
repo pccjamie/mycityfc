663bead2fbdf12afca0f561b065382b758e0f1ae
pccjamie <pccjamie@gmail.com>
2013-10-22 14:39:46 -0400

    rewrote video logic, reducing get requests for perf boost

--- app/controllers/fan_profiles_controller.rb
+++ app/controllers/fan_profiles_controller.rb
@@ -70,7 +70,7 @@ class FanProfilesController < ApplicationController
       @video_ids.push(item["id"]["videoId"])
     end
 
-    
+
           else
             p 'hi'
             # flash[:notice] = "Your team does not have a youtube channel. Here are league videos"
@@ -132,10 +132,7 @@ class FanProfilesController < ApplicationController
     year = Chronic.parse('this year').strftime('%Y')  #allows for new year to be passed in. In US soccer, season does not overlap years.
     url_mls = "http://www.mlssoccer.com/schedule?month=10&year=#{year}&club=all&competition_type=all&broadcast_type=all&op=Search&form_id=mls_schedule_form"
     schedule_array = Nokogiri::HTML(open(url_mls)).css('.schedule-table tbody tr').to_a
-
     @schedule = schedule_array
-
-    #get teams youtube username
 
   end
 
