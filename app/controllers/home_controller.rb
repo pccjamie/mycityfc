e91292d094a139af78dee807845af5e1a27d6b5d
pccjamie <pccjamie@gmail.com>
2013-11-13 12:14:05 -0500

    Routing changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 # skip_before_filter :authenticate_user!, :only => [:index]
 # before_filter :user_signed_in?
 
-before_filter :authenticate_user!, :except => [:index], :unless => :user_signed_in?
+before_filter :authenticate_user!, :except => [:splash], :unless => :user_signed_in?
 
 #no actual index view for now. this redirects to one of two paths after they've signed in and set up profile
 
@@ -11,17 +11,34 @@ before_filter :authenticate_user!, :except => [:index], :unless => :user_signed_
     current_user
   	if current_user.profile == 'Fan'
   		redirect_to :fan_profiles
-  	elsif 
+  	elsif
   		current_user.profile == 'Parent'
   		redirect_to :parent_profiles
-  	else
-  		redirect_to :splash
+  	# else
+  	# 	redirect_to :index
       # flash[:alert] = 'I am home. No profile.'
   	end
 	end
 
+
+  # def index
+  #   current_user
+  #   if current_user.profile == 'Fan'
+  #     redirect_to :fan_profiles
+  #   elsif 
+  #     current_user.profile == 'Parent'
+  #     redirect_to :parent_profiles
+  #   else
+  #     redirect_to :index
+  #     # flash[:alert] = 'I am home. No profile.'
+  #   end
+  # end
+
+
+
+
   def splash
-      redirect_to :splash
+    flash[:alert] = 'I am splashing'
   end
 
 end
