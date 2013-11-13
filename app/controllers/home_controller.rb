2ba1811bc4e9674eeb380f2116609243686943cf
pccjamie <pccjamie@gmail.com>
2013-11-13 11:44:21 -0500

    FB hash fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 # skip_before_filter :authenticate_user!, :only => [:index]
 # before_filter :user_signed_in?
 
-# before_filter :authenticate_user!, :except => [:splash], :unless => :user_signed_in?
+before_filter :authenticate_user!, :except => [:splash], :unless => :user_signed_in?
 
 #no actual index view for now. this redirects to one of two paths after they've signed in and set up profile
 
@@ -16,12 +16,12 @@ class HomeController < ApplicationController
   		redirect_to :parent_profiles
   	else
   		redirect_to :index
-      # flash[:alert] = 'Index page. No profile saved.'
+      # flash[:alert] = 'I am home. No profile.'
   	end
 	end
 
   def splash
-    flash[:alert] = 'Splash page'
+    flash[:alert] = 'I am splashing'
   end
 
 end
