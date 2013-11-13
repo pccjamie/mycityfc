61d63cb5fcfeaac90e5f6310699aeb12d24f9a6a
pccjamie <pccjamie@gmail.com>
2013-11-13 12:31:24 -0500

    Routing changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -29,7 +29,7 @@ before_filter :authenticate_user!, :except => [:splash], :unless => :user_signed
       user_signed_in? && current_user.profile == 'Parent'
       redirect_to :parent_profiles
     else
-      redirect_to :index
+      redirect_to :splash
       # flash[:alert] = 'I am home. No profile.'
     end
   end
